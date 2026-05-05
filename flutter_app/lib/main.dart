ff42141ff0706faaac33c7f1470947453ae526ac
dhananjaynikam21-blip <dhananjaynikam21@gmail.com>
2026-05-05 08:04:12 +0530

    feat(flutter): setup main.dart scaffold for KidLearn Play app

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,30 +1,18 @@
 import 'package:flutter/material.dart';
+import 'core/themes/app_theme.dart';
+import 'presentation/screens/home/home_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const KidLearnApp());
 
-class MyApp extends StatelessWidget {
+class KidLearnApp extends StatelessWidget {
+  const KidLearnApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'KidLearn Play',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('KidLearn Play')),
-      body: Center(
-        child: Text('Welcome to KidLearn Play!'),
-      ),
+      theme: AppTheme.lightTheme,
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
